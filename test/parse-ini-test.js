9bf341f19036d869cce438e2792da2fb7e8a312d
Imar Abreu Diaz <alu0100888447@ull.edu.es>
2019-03-28 09:05:07 +0000

    NPM create

---
+++ test/parse-ini-test.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var should = require("should");
+var parser = require('../lib/parse-ini.js');
+
+describe("parseINI", function () {
+  it("should parse a INI input", function () {
+    let expected = {
+      name: "Torres Quevedo",
+      address: {
+        invention: "The chess player"
+      }
+    };
+    let result = parseINI(`
+    name=Torres Quevedo
+    [address]
+    invention=The chess player`);
+    expected.should.eql(result);
+  });
+  it("should have an error if not valid", function () {
+    (function () {
+      parseINI('chazam')
+    }).should.throw(/Error/);
+  });
+})
