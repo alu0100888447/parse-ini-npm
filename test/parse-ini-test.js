1448425d65378da3b797778d40df45685c4a647b
Imar Abreu Diaz <abreudiazimar9620@gmail.com>
2019-03-30 14:34:47 +0000

    Test added

--- test/parse-ini-test.js
+++ test/parse-ini-test.js
@@ -18,9 +18,36 @@ describe("parseINI", () => {
     console.log(result);
     expected.should.eql(result);
   });
-/*  it("should have an error if not valid", function () {
-    (function () {
-      parser('chazam')
-    }).should.throw(/Error/);
-  });*/
+  it("LOTR", function () {
+    let expected = {
+      searchengine: 'https://duckduckgo.com/?q=$1',
+      spitefulness: '9.7',
+      gandalf:
+       { fullname: 'Mithrandir',
+         type: 'grey wizard',
+         website: 'http://tolkiengateway.net/wiki/Gandalf' },
+      gollum:
+       { fullname: 'Sméagol',
+         type: 'hobbit',
+         website: 'https://lotr.fandom.com/wiki/Gollum' } 
+    };
+    let result = parser(`
+    ; comments are preceded by a semicolon
+    ; global section
+    searchengine=https://duckduckgo.com/?q=$1
+    spitefulness=9.7
+
+    ; each section but the global starts with [identifier]
+    [gandalf]
+    fullname=Mithrandir
+    type=grey wizard
+    website=http://tolkiengateway.net/wiki/Gandalf
+
+    [gollum]
+    fullname=Sméagol
+    type=hobbit
+    website=https://lotr.fandom.com/wiki/Gollum`);
+    console.log(result);
+    expected.should.eql(result);
+  });
 })
